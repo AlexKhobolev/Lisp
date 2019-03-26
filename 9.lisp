02dd441b4535369ed881a928f56abbed1d169ba8
AlexKhobolev <44859059+AlexKhobolev@users.noreply.github.com>
2019-03-26 18:53:28 +0300

    Create 9.lisp

---
+++ 9.lisp
@@ -0,0 +1,24 @@
+Определите функцию, разделяющую исходный список на два подсписка. В
+первый из них должны попасть элементы с нечетными номерами, во второй —
+элементы с четными номерами.
+
+(defun split (arr)
+    ( 
+         (lambda (first second rest) 
+             (
+                 cond
+                    ( (null arr) (list NIL NIL))
+                    ((null second) (list (list first) NIL))
+                    (T (list 
+                            (cons first (car (split rest))) 
+                            (cons second (cadr (split rest))) 
+                       )
+                    )
+             )
+        ) (car arr) (cadr arr) (cddr arr)
+    )
+)
+
+(print (split '( 1 2 3 4 5 6 7 8 9 ))) ;((1 3 5 7 9) (2 4 6 8)) 
+(print (split '( -10))) ;((-10) NIL) 
+(print (split NIL)) ;(NIL NIL) 
