d044f4e822f6bc8142e72976a35a134d3b5db189
AlexKhobolev <44859059+AlexKhobolev@users.noreply.github.com>
2019-03-27 22:34:41 +0300

    Update 19.lisp

--- 19.lisp
+++ 19.lisp
@@ -5,3 +5,14 @@
   (if (zerop n) m (list (onion-n (1- n) m))))
 
 (print (onion-n 7)); (((((((7)))))))
+
+
+
+
+(defun onion-n (n)
+  (cond
+        ((null n) nil)
+        (t (if (eq n 1) (list n))
+           )))
+
+(print (onion-n 1))
