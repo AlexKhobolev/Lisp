d4c9986435d2e3caa0fd1139768f597db5ab98fe
AlexKhobolev <44859059+AlexKhobolev@users.noreply.github.com>
2019-03-28 07:29:53 +0200

    Delete 39.lisp

--- 39.lisp
+++
@@ -1,8 +0,0 @@
-Определите функцию СИММЕТРИЧЕСКАЯ-РАЗНОСТЬ, формирующую множество из
-элементов не входящих в оба множества.
-
-(defun sym-diff (s1 s2)
-  (append (remove-if #'(lambda (x) (member x s2)) s1)
-          (remove-if #'(lambda (x) (member x s1)) s2)))
-
-(print (sym-diff '(1 2 3 5) '(6 4 1 8 3))); (2 5 6 4 8) 
