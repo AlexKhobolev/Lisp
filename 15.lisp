faf61c57c09c72b0dfe5f2d994a826084fa3553c
AlexKhobolev <44859059+AlexKhobolev@users.noreply.github.com>
2019-03-28 07:52:43 +0200

    Update 15.lisp

--- 15.lisp
+++ 15.lisp
@@ -2,14 +2,13 @@
 ;Определите функцию, вычисляющую скалярное произведение векторов, 
 ;заданных списками целых чисел.
 
-
 (defun scal-prod (v1 v2)
-   (if (or (null v1)(null v2)) 0   
-   (+ (* (car v1) (car v2)) ( scal-prod(cdr v1) (cdr v2)))
-   )
+    (cond
+         ((null v1) nil)
+         ((null v2) nil)
+         (t (+ (* (car v1) (car v2)) (* (cadr v1) (cadr v2))))
+    )
 )
-
-(print(scal-prod '(4 7) '(2 9))); 71
-
+(print(scal-prod '(4 7) '(2 9)))
 
 
