a83d47416ade9f7bdbe85d92a4b8d953bd0e9af5
AlexKhobolev <44859059+AlexKhobolev@users.noreply.github.com>
2019-04-01 16:11:53 +0300

    Update 19.lisp

--- 19.lisp
+++ 19.lisp
@@ -1,12 +1,11 @@
 ;Определите функцию (ЛУКОВИЦА n), строящую N-уровневый вложенный список, 
 ;элементом которого на самом глубоком уровне является N.
 
-(defun onion-n (n &optional (m n))
-  (cond
-        ((eq n 0) 0)
-        (t (list (if (eq n 1) (ret m) (onion-n (1- n)))) )
-           ))
-(defun ret (n) 
-        (n)
-    )
-(print (onion-n 4))
+(defun onion (n &optional (m 1) (l '(1))) 
+    (if (= n m)l           
+        (cons (onion n (+ 1 m) (cons n ())) () 
+         )
+     )
+)
+
+(print(onion 3))
