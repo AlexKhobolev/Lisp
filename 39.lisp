7b46104f84f9a2d476d800d5e821422250c1f564
AlexKhobolev <44859059+AlexKhobolev@users.noreply.github.com>
2019-04-08 20:46:49 +0300

    Create 39.lisp

---
+++ 39.lisp
@@ -0,0 +1,21 @@
+;39
+;Определите функцию СИММЕТРИЧЕСКАЯ-РАЗНОСТЬ, формирующую множество из
+;элементов не входящих в оба множества.
+
+(defun our-set-exclusive-or (w v)
+  (list (unique w v) (unique v w)))
+
+(defun is-member (el arr)
+    (cond
+        ((null arr) NIL)
+        ((= (car arr) el) T)
+        (t (is-member el (cdr arr)))
+    )
+)
+ 
+(defun unique (w v)
+  (cond ((null w) nil)
+        ((is-member (car w) v) (unique (cdr w) v))
+        ((cons (car w) (unique (cdr w) v)))))
+ 
+(print (our-set-exclusive-or '(1 2 3 4 6) '(2 3 4 5 7)))
