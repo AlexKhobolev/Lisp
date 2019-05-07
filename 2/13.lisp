3a4b3442e883d249143695dbc15fbe3fe11b8753
AlexKhobolev <44859059+AlexKhobolev@users.noreply.github.com>
2019-05-07 18:51:07 +0300

    Update 13.lisp

--- 2/13.lisp
+++ 2/13.lisp
@@ -1,9 +1,9 @@
 ; 13.Определите функцию, которая возвращает в качестве значения свое определение (лямбда-выражение)
 
-(defun return-lambda (x)
+(defun return (x)
     ((lambda (x) x)
         '(lambda (x) x))
 )
 
 
-(print(return-lambda '(x))); (LAMBDA (X) X)
+(print(return '(x))); (LAMBDA (X) X)
