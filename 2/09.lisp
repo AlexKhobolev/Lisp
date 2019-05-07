119260556c5811b03b5955ea9b762bc8352217ae
AlexKhobolev <44859059+AlexKhobolev@users.noreply.github.com>
2019-05-07 18:49:20 +0300

    Update 09.lisp

--- 2/09.lisp
+++ 2/09.lisp
@@ -1,12 +1,12 @@
-; 9) 9. Напишите генератор порождения чисел Фибоначчи: 0, 1, 1, 2, 3, 5, ...
+; 9. Напишите генератор порождения чисел Фибоначчи: 0, 1, 1, 2, 3, 5, ...
 
 (defun fib ()
 	(let
-		((prev 0) (cur 1) (temp 0))
+		((x 0) (y 1) (temp 0))
 		(lambda () (and
-			(setq temp cur)
-			(setq cur (+ prev cur))
-			(setq prev temp)
+			(setq temp y)
+			(setq y (+ x y))
+			(setq x temp)
 			temp
 		))
 	)
@@ -14,3 +14,14 @@
 
 (setq g1 (fib))
 (setq g2 (fib))
+
+(print (funcall g1));1
+(print (funcall g2));1
+(print (funcall g1));1
+(print (funcall g2));1
+(print (funcall g1));2
+(print (funcall g2));2
+(print (funcall g1));3
+(print (funcall g2));3
+(print (funcall g1));5
+(print (funcall g2));5
